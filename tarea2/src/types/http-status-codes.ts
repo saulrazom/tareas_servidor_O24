b53a6d8aa789c661d7f77387db7a913e45472b84
URSUS32 <saul.razo@iteso.mx>
2024-10-07 20:57:29 -0600

    new branch tarea3

--- tarea2/src/types/http-status-codes.ts
+++
@@ -1,8 +0,0 @@
-export enum HTTP_STATUS_CODES {
-    SUCCES        = 200,
-    BAD_REQUEST   = 400,
-    AUTHORIZATION = 401,
-    FORBIDDEN     = 403,
-    NOT_FOUND     = 404,
-    SERVER_ERROR  = 500
-}
